caee44ef1b7b47538d478b1a56cc2bda80b76af3
Paulo Barbosa <p.barbosa088@gmail.com>
2019-05-19 17:53:04 -0300

    Terminado primeira versão mostravel

--- Car_destruction.py
+++ Car_destruction.py
@@ -8,19 +8,22 @@ pygame.init()
 largura = 600
 altura = 600
 
+t_player = 20
+
 #cores interface
-azul = (99, 184, 255)
-azul_claro = (126, 192, 238)
-preto = (0, 0, 0);
-branco = (255, 255, 255)
-vermelho = (255, 0, 0)
+azul = (99, 184, 255, 255)
+azul_claro = (126, 192, 238, 255)
+preto = (0, 0, 0, 255)
+branco = (255, 255, 255, 255)
+vermelho = (255, 0, 0, 255)
+verde = (0, 255, 0, 255)
 
 class Player:
-    def __init__(self, x, y, cor, cor_parede):
+    def __init__(self, x, y, cor, nome):
         self.x = x
         self.y = y
         self.cor = cor
-        self.cor_parede = cor_parede
+        self.nome = nome
         self.j = 1
         self.x_ant_1 = x
         self.y_ant_1 = y
@@ -44,46 +47,85 @@ class Player:
             self.x_ant_3 = self.x
             self.y_ant_3 = self.y
 
-            pygame.draw.rect(fundo, self.cor_parede, [self.x_ant_1, self.y_ant_1, 15, 15])
-            pygame.draw.rect(fundo, self.cor_parede, [self.x_ant_2, self.y_ant_2, 15, 15])
-            pygame.draw.rect(fundo, self.cor_parede, [self.x_ant_3, self.y_ant_3, 15, 15])
+            pygame.draw.rect(fundo, self.cor, [self.x_ant_1, self.y_ant_1, t_player, t_player])
+            pygame.draw.rect(fundo, self.cor, [self.x_ant_2, self.y_ant_2, t_player, t_player])
+            pygame.draw.rect(fundo, self.cor, [self.x_ant_3, self.y_ant_3, t_player, t_player])
           
         self.j +=1
         
         a = randint(0,3)
         
         if a == 0:
-            if temp_x + 40 > 500 :
+            if temp_x + t_player > 500 :
                 self.mover()
             else:
-                self.x += 40
-                
+                if fundo.get_at((temp_x + t_player, temp_y)) == self.cor or fundo.get_at((temp_x + t_player, temp_y)) == branco:
+                    self.x += t_player
+                    pygame.draw.rect(fundo, self.cor, [self.x, self.y, t_player, t_player])
+                    return True
+                else:
+                    pygame.draw.rect(fundo, branco, [self.x_ant_1, self.y_ant_1, t_player, t_player])
+                    pygame.draw.rect(fundo, branco, [self.x_ant_2, self.y_ant_2, t_player, t_player])
+                    pygame.draw.rect(fundo, branco, [self.x_ant_3, self.y_ant_3, t_player, t_player])
+                    pygame.draw.rect(fundo, branco, [self.x, self.y, t_player, t_player])
+                    return False
+                    
         elif a == 1:
-            if temp_x - 40 < 140 :
+            if temp_x - t_player < 140 :
                 self.mover()
             else:
-                self.x -= 40
+                if fundo.get_at((temp_x - t_player, temp_y)) == self.cor or fundo.get_at((temp_x - t_player, temp_y)) == branco:
+                    self.x -= t_player
+                    pygame.draw.rect(fundo, self.cor, [self.x, self.y, t_player, t_player])
+                    return True
+                else:
+                    pygame.draw.rect(fundo, branco, [self.x_ant_1, self.y_ant_1, t_player, t_player])
+                    pygame.draw.rect(fundo, branco, [self.x_ant_2, self.y_ant_2, t_player, t_player])
+                    pygame.draw.rect(fundo, branco, [self.x_ant_3, self.y_ant_3, t_player, t_player])
+                    pygame.draw.rect(fundo, branco, [self.x, self.y, t_player, t_player])
+                    return False
 
         elif a == 2:
-            if temp_y + 40 > 460:
+            if temp_y + t_player > 460:
                 self.mover()
             else:
-                self.y += 40
+                if fundo.get_at((temp_x, temp_y + t_player)) == self.cor or fundo.get_at((temp_x, temp_y + t_player)) == branco:
+                    self.y += t_player
+                    pygame.draw.rect(fundo, self.cor, [self.x, self.y, t_player, t_player])
+                    return True
+                else:
+                    pygame.draw.rect(fundo, branco, [self.x_ant_1, self.y_ant_1, t_player, t_player])
+                    pygame.draw.rect(fundo, branco, [self.x_ant_2, self.y_ant_2, t_player, t_player])
+                    pygame.draw.rect(fundo, branco, [self.x_ant_3, self.y_ant_3, t_player, t_player])
+                    pygame.draw.rect(fundo, branco, [self.x, self.y, t_player, t_player])
+                    return False
 
         elif a == 3:
-            if temp_y - 40 < 180:
+            if temp_y - t_player < 180:
                 self.mover()
             else:
-                self.y -= 40
+                if fundo.get_at((temp_x, temp_y - t_player)) == self.cor or fundo.get_at((temp_x, temp_y - t_player)) == branco:
+                    self.y -= t_player
+                    pygame.draw.rect(fundo, self.cor, [self.x, self.y, t_player, t_player])
+                    return True
+                else:
+                    pygame.draw.rect(fundo, branco, [self.x_ant_1, self.y_ant_1, t_player, t_player])
+                    pygame.draw.rect(fundo, branco, [self.x_ant_2, self.y_ant_2, t_player, t_player])
+                    pygame.draw.rect(fundo, branco, [self.x_ant_3, self.y_ant_3, t_player, t_player])
+                    pygame.draw.rect(fundo, branco, [self.x, self.y, t_player, t_player])
+                    return False
 
-        pygame.draw.rect(fundo, self.cor, [self.x, self.y, 15, 15])
 
-
-
+ps = []
 #player 1
-p1 = Player(140, 180, azul_claro, preto)
+p1 = Player(140, 180, azul_claro, "Azul")
+ps.append(p1)
 #player 2
-p2 = Player(500, 460, vermelho, preto)
+p2 = Player(500, 460, vermelho, "Vermelho")
+ps.append(p2)
+#player 3
+p3 = Player(220, 260, verde, "Verde")
+ps.append(p3)
 
 #criação da tela
 fundo = pygame.display.set_mode((largura, altura));
@@ -111,76 +153,31 @@ def criar_cenario():
 
 def criar_cenario2():
     pygame.draw.lines(fundo, preto,False, [(120,160),(120,480),(520,480),(520,160),(120,160)], 5)
-    #linhas horizontais
-##    pygame.draw.lines(fundo,preto,False,[(120,200),(520,200)],5)
-##    pygame.draw.lines(fundo,preto,False,[(120,240),(520,240)],5)
-##    pygame.draw.lines(fundo,preto,False,[(120,280),(520,280)],5)
-##    pygame.draw.lines(fundo,preto,False,[(120,320),(520,320)],5)
-##    pygame.draw.lines(fundo,preto,False,[(120,360),(520,360)],5)
-##    pygame.draw.lines(fundo,preto,False,[(120,400),(520,400)],5)
-##    pygame.draw.lines(fundo,preto,False,[(120,440),(520,440)],5)
-   #linhas verticais
-##    pygame.draw.lines(fundo,preto,False,[(160,160),(160,480)],5)
-##    pygame.draw.lines(fundo,preto,False,[(200,160),(200,480)],5)
-##    pygame.draw.lines(fundo,preto,False,[(240,160),(240,480)],5)
-##    pygame.draw.lines(fundo,preto,False,[(280,160),(280,480)],5)
-##    pygame.draw.lines(fundo,preto,False,[(320,160),(320,480)],5)
-##    pygame.draw.lines(fundo,preto,False,[(360,160),(360,480)],5)
-##    pygame.draw.lines(fundo,preto,False,[(400,160),(400,480)],5)
-##    pygame.draw.lines(fundo,preto,False,[(440,160),(440,480)],5)
-##    pygame.draw.lines(fundo,preto,False,[(480,160),(480,480)],5)
-    
-def mov_random(a, b):
-    x = int(a)
-    y = int(b)
-    #limites (140,180) / (140,460) / (500,460) / (500,180)
-    #x entre 140 e 500 y entre 180 e 460
-    a = randint(0,3)
-    if a == 0:
-        if x + 40 > 500 :
-            mov_random(a,b)
-        else:
-            x += 40
-            return "x+40"
-            
-    elif a == 1:
-        if x - 40 < 140:
-            mov_random(a,b)
-        else:
-            x -= 40
-            return "x-40"
 
-    elif a == 2:
-        if y + 40 > 460:
-            mov_random(a,b)
-        else:
-            y += 40
-            return "y+40"
-            
-
-    elif a == 3:
-        if y - 40 < 180:
-            mov_random(a,b)
-        else:
-            y -= 40
-            return "y-40"
-            
-   
-    
 #Programa sempre ficar rodando e desenhando     
 cond = True;
+
 while cond:
     #capturar eventos do mouse
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            cond = False;
+            cond = False
     fundo.fill(branco)
     criar_cenario2()
+
+    i = 0
+    while (i < len(ps)):
+        if ps[i].mover() == False:
+            ps.remove(ps[i])
+        i += 1
+
+    if len(ps) == 1:
+        pygame.font.init()
+        fonte = pygame.font.get_default_font()
+        fonte_ganhador = pygame.font.SysFont(fonte, 45)
+        text = fonte_ganhador.render(ps[0].nome, 1 , ps[0].cor)
+        fundo.blit(text, (300, 300))
     
-    p1.mover()
-    p2.mover()
-    
-    print(randint(0,3))
     time.sleep(0.1)
 
     pygame.display.flip()
